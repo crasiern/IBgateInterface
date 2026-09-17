1d517af7e0ebdfd392bf55bf1aaba018b5ea3ac6
crasiern <crasiern@gmail.com>
2026-09-17 02:53:03 -0600

    git initalized; base project working

---
+++ IBgateInterface.py
@@ -0,0 +1,59 @@
+from ib_async import *
+import datetime as dt
+import sys
+
+"""
+Responsibilities of this code
+
+Handles the interactions with IB Gateway to fetch historic data
+of a specifc contract. Also handles the timing?
+
+
+"""
+def main():
+     stockTicker = sys.argv[1]
+     returnMaxStockChart(stockTicker)
+
+# get stock prices as far back as possible
+# adjusted for dividends and stock splits
+def returnMaxStockChart(ticker):
+    ib = IB()
+    try:
+        ib.connect('127.0.0.1', 4001, clientId=1)
+    except ConnectionError:
+        print("IB Gateway not functioning")
+        sys.exit(1)
+    except TimeoutError:
+            print("Timed Out")
+            sys.exit(2)
+
+    stock = Stock(ticker, 'SMART', 'USD')
+    current_date = dt.datetime.now()
+    target_year = ib.reqHeadTimeStamp(stock, whatToShow='ADJUSTED_LAST', useRTH=False).year
+
+    year_incrments = 10
+    index_year = int(current_date.year)
+    index_date = dt.date(index_year, 1, 1)
+
+    while True:
+        if index_date.year < target_year:
+                    break
+        tenYearChunck = ib.reqHistoricalData(
+            stock, endDateTime=index_date, 
+            durationStr='10 Y',
+            barSizeSetting='1 day', 
+            whatToShow='TRADES', 
+            useRTH=False
+        )
+        chunkToCSV(tenYearChunck, ticker)
+        index_date = index_date.replace(year=index_date.year - year_incrments)
+
+# probably should make with filepath in mind
+def chunkToCSV(tenYearChunck, ticker):
+      with open(f"{ticker}.csv", 'a') as f: 
+        for i in range(len(tenYearChunck) - 1, 0, -1):
+            f.write(f"{tenYearChunck[i].date},{tenYearChunck[i].open},{tenYearChunck[i].high},{tenYearChunck[i].low},{tenYearChunck[i].close},{tenYearChunck[i].volume},{tenYearChunck[i].average}")
+            f.write('\n')
+
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,15 @@
+# IBScrapeSuite
+
+
+
+
+
+## IKBR Limitations
+-Bars whose size is 30 seconds or less older than six months
+-Expired futures data older than two years counting from the future's expiration date.
+-Expired options, FOPs, warrants and structured products.
+-End of Day (EOD) data for options, FOPs, warrants and structured products.
+-Data for expired future spreads
+-Data for securities which are no longer trading.
+-Native historical data for combos. Historical data is not stored in the IB database separately for combos.; combo historical data in TWS or the API is the sum of data from the legs.
+-Historical data for securities which move to a new exchange will often not be available prior to the time of the move.
